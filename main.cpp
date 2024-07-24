07b457158aa23b36dc522901a1aba684583c4f3b
Richard Kroesen <kroesen.ri@gmail.com>
2024-07-24 15:50:28 +0200

    Add GoogleTesting framework configuration.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
-template <typename T>
-const static int linearSearch(const std::vector<T>& arr, const T& key) {
-    auto it = std::find(arr.begin(), arr.end(), key);
-    
-    if (it != arr.end()) {
-        return std::distance(arr.begin(), it);
-    } else {
-        return -1;
-    }
-}
+#include "linear_search.hpp"
 
 int main() {
     std::vector<int> intArr = {10, 25, 37, 42, 54};
