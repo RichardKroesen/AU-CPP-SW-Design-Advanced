708163cc912026e93820f1098123b778b1307be3
Richard Kroesen <kroesen.ri@gmail.com>
2024-07-24 16:25:40 +0200

    Enh added additional data size as samples for better measurements. Add random generator helper function with Mersenne Twister pseudo-random generator.

--- Tests/exercise_tests.cpp
+++ Tests/exercise_tests.cpp
@@ -1,46 +1,75 @@
 #include <gtest/gtest.h>
 #include <chrono>
+#include <random>
 
 #include "../linear_search.hpp" // Code under test (CUD)
 
+std::vector<int> generateRandomVector(size_t size, const int min, const int max, unsigned int seed) {
+    std::vector<int> vec(size);
+    std::mt19937 rng(seed); // Mersenne Twister pseudo-random generator, source: https://www.geeksforgeeks.org/stdmt19937-class-in-cpp/ 
+    std::uniform_int_distribution<int> dist(min, max);
+
+    for (auto& el : vec) {
+        el = dist(rng);
+    }
+
+    return vec;
+}
 
 TEST(LinearSearchTest, FindSevenInMiddle) {
-    std::vector<int> intArr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    const int actual_position = 6;
+    // Data Arrange
+    std::vector<int> intArr = generateRandomVector(10'000'000, 10, 20, 999);
+    constexpr int actual_position = 5'000'000;
 
+    // Act
+    intArr[5'000'000] = 7;
+
+    // Check
     auto start = std::chrono::high_resolution_clock::now();
     EXPECT_EQ(linearSearch(intArr, 7), actual_position);
     auto end = std::chrono::high_resolution_clock::now();
+
+    // Results Printing
     std::chrono::duration<double> elapsed = end - start;
     std::cout << "FindSevenInMiddle took " << elapsed.count() << " seconds." << std::endl;
 }
 
 TEST(LinearSearchTest, NoSevenInVector) {
-    std::vector<int> intArr = {1, 2, 3, 4, 5, 6, 8, 9, 10, 11};
+    std::vector<int> intArr = generateRandomVector(10'000'000, 10, 20, 999);
+    constexpr int actual_position = -1;
 
     auto start = std::chrono::high_resolution_clock::now();
-    EXPECT_EQ(linearSearch(intArr, 7), -1);
+    EXPECT_EQ(linearSearch(intArr, 7), actual_position);
     auto end = std::chrono::high_resolution_clock::now();
+
+    // Results Printing
     std::chrono::duration<double> elapsed = end - start;
     std::cout << "NoSevenInVector took " << elapsed.count() << " seconds." << std::endl;
 }
 
 TEST(LinearSearchTest, FindElementLessThanSevenInMiddle) {
-    std::vector<int> intArr = {8, 9, 10, 5, 11, 12, 13};
+    std::vector<int> intArr = generateRandomVector(10'000'000, 10, 20, 999); 
+    constexpr int actual_position = 5'000'000;
+
+    intArr[5'000'000] = 5; // Set the middle element to 5
 
     auto start = std::chrono::high_resolution_clock::now();
-    EXPECT_EQ(linearSearch(intArr, 5), 3);
+    EXPECT_EQ(linearSearch(intArr, 5), actual_position);
     auto end = std::chrono::high_resolution_clock::now();
+
+    // Results Printing
     std::chrono::duration<double> elapsed = end - start;
     std::cout << "FindElementLessThanSevenInMiddle took " << elapsed.count() << " seconds." << std::endl;
 }
 
 TEST(LinearSearchTest, NoElementLessThanSevenInVector) {
-    std::vector<int> intArr = {8, 9, 10, 11, 12, 13, 14};
+    std::vector<int> intArr = generateRandomVector(10000000, 10, 20, 999);
 
     auto start = std::chrono::high_resolution_clock::now();
     EXPECT_EQ(linearSearch(intArr, 5), -1);
     auto end = std::chrono::high_resolution_clock::now();
+
+    // Results Printing
     std::chrono::duration<double> elapsed = end - start;
     std::cout << "NoElementLessThanSevenInVector took " << elapsed.count() << " seconds." << std::endl;
 }
